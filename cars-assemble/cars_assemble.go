1dd5f25d20170e7f789a948334983d6398a9351d
le-rich <richardle1997@gmail.com>
2021-12-19 20:19:42 -0800

    Exercise 5 complete

--- cars-assemble/cars_assemble.go
+++ cars-assemble/cars_assemble.go
@@ -1,25 +1,40 @@
 package cars
 
+import (
+	"math"
+)
+
 // SuccessRate is used to calculate the ratio of an item being created without
 // error for a given speed
 func SuccessRate(speed int) float64 {
-	panic("SuccessRate not implemented")
+	switch {
+	case speed == 0:
+		return 0.0
+	case speed <= 4:
+		return 1.0
+	case speed <= 8:
+		return 0.9
+	case speed <= 10:
+		return 0.77
+	default:
+		return -1.0
+	}
 }
 
 // CalculateProductionRatePerHour for the assembly line, taking into account
 // its success rate
 func CalculateProductionRatePerHour(speed int) float64 {
-	panic("CalculateProductionRatePerHour not implemented")
+	return float64(speed) * 221.0 * SuccessRate(speed)
 }
 
 // CalculateProductionRatePerMinute describes how many working items are
 // produced by the assembly line every minute
 func CalculateProductionRatePerMinute(speed int) int {
-	panic("CalculateProductionRatePerMinute not implemented")
+	return int(math.Floor(CalculateProductionRatePerHour(speed)) / 60)
 }
 
 // CalculateLimitedProductionRatePerHour describes how many working items are
 // produced per hour with an upper limit on how many can be produced per hour
 func CalculateLimitedProductionRatePerHour(speed int, limit float64) float64 {
-	panic("CalculateLimitedProductionRatePerHour not implemented")
+	return math.Min(CalculateProductionRatePerHour(speed), limit)
 }
