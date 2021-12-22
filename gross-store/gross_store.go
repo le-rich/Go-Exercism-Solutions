ebf7fb63abeeff39d1930cfad4b0faf4c65b029b
le-rich <richardle1997@gmail.com>
2021-12-22 04:58:19 -0800

    Maps practice

---
+++ gross-store/gross_store.go
@@ -0,0 +1,63 @@
+package gross
+
+// Units stores the Gross Store unit measurements.
+func Units() map[string]int {
+	m := make(map[string]int)
+	m["quarter_of_a_dozen"] = 3
+	m["half_of_a_dozen"] = 6
+	m["dozen"] = 12
+	m["small_gross"] = 120
+	m["gross"] = 144
+	m["great_gross"] = 1728
+
+	return m
+}
+
+// NewBill creates a new bill.
+func NewBill() map[string]int {
+	return make(map[string]int)
+}
+
+// AddItem adds an item to customer bill.
+func AddItem(bill, units map[string]int, item, unit string) bool {
+	if _, ok := units[unit]; !ok {
+		return false
+	}
+
+	if _, ok := bill[item]; ok {
+		bill[item] += units[unit]
+	} else {
+		bill[item] = units[unit]
+	}
+
+	return true
+}
+
+// RemoveItem removes an item from customer bill.
+func RemoveItem(bill, units map[string]int, item, unit string) bool {
+	if _, ok := units[unit]; !ok {
+		return false
+	}
+
+	if billVal, ok := bill[item]; !ok {
+		return false
+	} else if (billVal - units[unit]) < 0 {
+		billVal -= units[unit]
+		return false
+	} else if (billVal - units[unit]) == 0 {
+		delete(bill, item)
+		return true
+	} else {
+		bill[item] -= units[unit]
+		return true
+	}
+}
+
+// GetItem returns the quantity of an item that the customer has in his/her bill.
+func GetItem(bill map[string]int, item string) (int, bool) {
+	if _, ok := bill[item]; !ok {
+		return 0, false
+	}
+
+	return bill[item], true
+}
